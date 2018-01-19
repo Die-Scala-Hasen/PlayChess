c1df3d8bf2eaa71a89542059981c81a88c32dc8e
Marco <marco.boxler@htwg-konstanz.de>
2018-01-19 20:26:44 +0100

    Layout and add some buttons

--- public/javascripts/chess.js
+++ public/javascripts/chess.js
@@ -6,6 +6,21 @@ var socket;
 
 var full = '{"gamefield":[{"figur":"bTower","x":0,"y":0},{"figur":"bKnight","x":1,"y":0},{"figur":"bBishop","x":2,"y":0},{"figur":"bQueen","x":3,"y":0},{"figur":"bKing","x":4,"y":0},{"figur":"bBishop","x":5,"y":0},{"figur":"bKnight","x":6,"y":0},{"figur":"bTower","x":7,"y":0},{"figur":"bPawn","x":0,"y":1},{"figur":"bPawn","x":1,"y":1},{"figur":"bPawn","x":2,"y":1},{"figur":"bPawn","x":3,"y":1},{"figur":"bPawn","x":4,"y":1},{"figur":"bPawn","x":5,"y":1},{"figur":"bPawn","x":6,"y":1},{"figur":"bPawn","x":7,"y":1},{"figur":"wPawn","x":0,"y":6},{"figur":"wPawn","x":1,"y":6},{"figur":"wPawn","x":2,"y":6},{"figur":"wPawn","x":3,"y":6},{"figur":"wPawn","x":4,"y":6},{"figur":"wPawn","x":5,"y":6},{"figur":"wPawn","x":6,"y":6},{"figur":"wPawn","x":7,"y":6},{"figur":"wTower","x":0,"y":7},{"figur":"wKnight","x":1,"y":7},{"figur":"wBishop","x":2,"y":7},{"figur":"wQueen","x":3,"y":7},{"figur":"wKing","x":4,"y":7},{"figur":"wBishop","x":5,"y":7},{"figur":"wKnight","x":6,"y":7},{"figur":"wTower","x":7,"y":7}]}'
 
+var numberChar = {
+    0: "-",
+    1: "A",
+    2: "B",
+    3: "C",
+    4: "D",
+    5: "E",
+    6: "F",
+    7: "G",
+    8: "H"
+};
+
+function mapper (propertyName) {
+    return numberChar[propertyName];
+};
 
 function start() {
     socket = new WebSocket("ws://localhost:9000/socket");
@@ -47,21 +62,37 @@ function drawGamefield(json) {
     for (var i = 0; i < 9; i++) {
         var zdiv = document.createElement("DIV");
         var maindiv = document.getElementById("mydiv")
-        // document.body.appendChild(maindiv);
         for (n = 0; n < 9; n++) {
             if(i == 0){
                 var btn = document.createElement("BUTTON");
                 btn.className = "Chessbtn"
-                 var t = document.createTextNode(""+n);
-                 btn.appendChild(t);
-                zdiv.appendChild(btn);
+                btn.backgroundColor = "white"
+                    var t = document.createTextNode("" + mapper(n));
+                    btn.appendChild(t);
+                    zdiv.appendChild(btn);
+
             } else
             drawChessPattern(i,n)
         }
         maindiv.appendChild(zdiv);
     }
 
-     placeFigure(json)
+    placeFigure(json);
+    placeSideCoordinates();
+
+
+    function placeSideCoordinates() {
+        for (var i = 0; i < 8; i++) {
+            var btn = document.getElementById(i + "_0");
+            // btn.id = i + "_A";
+            btn.className = "img" + (i+1)
+            btn.disabled = "disabled";
+            // btn.style.backgroundColor="grey";
+            // //var t = document.createTextNode(""+(i+1));
+            // // btn.innerHTML = ""+(i+1);
+            // btn.textContent = "test"
+        }
+    }
 
     function placeFigure(json) {
         for (var i = 0; i < json.gamefield.length; i++) {
